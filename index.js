3aebd4141935930333109a7c276165e31ffbd180
kalle-1413@hotmail.com <kalle-1413@hotmail.com>
2023-04-19 23:56:37 -0500

    mejora total

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 function themeChange (){
-  
-  
   const theme = document.getElementById("thema").classList.toggle("ligth-theme")
 }
+function obtenerHora(){
+  const reloj= document.getElementById('reloj');
+  const relojHr= document.getElementById('hr');
+  const relojMn= document.getElementById('mn');
+  const relojSg= document.getElementById('sg');
+  const relojAm= document.getElementById('am');
+
+
+
+  const fecha= new Date();
+  const hora = (fecha.getHours()>12)?fecha.getHours()-12:fecha.getHours();
+  const minutos=fecha.getMinutes();
+  const hr = (hora.toString().length==1)?"0"+hora:hora;
+  const mn= (minutos.toString().length==1)?"0"+minutos:minutos;
+  const segundos= fecha.getSeconds();
+  const sg= (segundos.toString().length==1)?"0"+segundos:segundos;
+  const am =( fecha.getHours()>12) ? 'PM':'AM';
+  relojHr.textContent=hr;
+  relojMn.textContent=mn;
+  relojSg.textContent=sg;
+  relojAm.textContent=am;
+}
+setInterval(obtenerHora, 1000);
